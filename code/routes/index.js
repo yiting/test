ba2644e5a64914a5423144afe60f371aa8d23197
stevenpan <stevenpan@tencent.com>
2018-11-21 11:27:35 +0800

    样式更新

--- code/routes/index.js
+++ code/routes/index.js
@@ -6,13 +6,13 @@ const router = express.Router();
 //渲染页面路由
 router.get("/", function(req, res, next) {
   res.render("start", {
-    title: "设计编译 - 内测版"
+    title: "视觉编译 - 内测版"
   });
 });
 
 router.get("/index", function(req, res, next) {
   res.render("index", {
-    title: "设计编译 - 内测版"
+    title: "视觉编译 - 内测版"
   });
 });
 
