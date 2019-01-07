3986f2a0c2d2e609e1ad4e72f9eb2643b3c7dbf5
alltasxiao <alltasxiao@tencent.com>
2019-01-07 18:58:25 +0800

    更新背景图z-index

--- code/server_modules/render/h5/css_dom.js
+++ code/server_modules/render/h5/css_dom.js
@@ -597,7 +597,7 @@ class CssDom {
             let value = this[key];
 
             if (value !== null) {
-                if (!isNaN(value) && key != "opacity") { // 数字的话进行单位转换
+                if (!isNaN(value) && key != "opacity" && key != "zIndex") { // 数字的话进行单位转换
                     value = _transUnit(value);
                 }
 
