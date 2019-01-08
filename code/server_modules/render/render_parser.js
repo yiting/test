6f0ae5ddad1ea447501d2786ca5cc119734e1851
yixionglin <yixionglin@tencent.com>
2019-01-08 11:14:27 +0800

    update

--- code/server_modules/render/render_parser.js
+++ code/server_modules/render/render_parser.js
@@ -12,8 +12,8 @@ let parse = function (dslTree, platformType) {
     return DSLTreeProcessor.parseTree(dslTree, platformType);
 }
 
-const QNODE_TYPES = ['QImage', 'QText', 'QIcon', 'QShape', 'QWidget'];
-const QCONTAINER_TYPES = ['QBody', 'QLayer'];
+const QNODE_TYPES = ['QImage','QText','QIcon','QShape','QWidget'];
+const QCONTAINER_TYPES = ['QBody','QLayer'];
 class DSLTreeProcessor {
     /**
      * 
@@ -29,49 +29,32 @@ class DSLTreeProcessor {
         delete _tree.root;
         return _tree;
     }
-    static _parseNode(platformType, dslTree, pnode) {
-        if (pnode.type === 'QBody');
-        if (!pnode.children || !pnode.children.length) return;
-        pnode.children.forEach((node, index) => {
-            let element = null;
-            if (~QCONTAINER_TYPES.indexOf(node.type)) { // 容器元素转换
-                element = this._convertNode(node, platformType);
-            } else if (~QNODE_TYPES.indexOf(node.type)) { // 模型节点解析&拼装
-                element = this._parseWidgetNode(node, dslTree, platformType)
-            }
+    static _parseNode(platformType,dslTree,pnode) { 
+        if(!pnode.children || !pnode.children.length) return;
+        pnode.children.forEach((node,index) => {
+            let element = (node.modelName != 'layer') ? this._parseWidgetNode(node,dslTree,platformType) : this._convertNode(node,platformType);
             pnode.children[index] = element;
-
         })
     }
-    static _parseWidgetNode(node, dslTree, platformType) {
-        // TODO
-        let matchData = dslTree.getModelData(node.id);
-        let jsonNode = matchData.getMatchNode();
-        let styles = {};
-        // 单节点才获取样式,否则node上没有样式
-        if (jsonNode['0'] && !jsonNode['1']) {
-            styles = jsonNode['0'].styles;
-        }
 
-        Object.assign(matchData, {
-            constraints: node.constraints,
-            styles: styles
-        });
-        return walkModel(matchData, function (mdata, structure) {
-            let {
-                constraints,
-                styles,
-                modelName
-            } = mdata;
-            let template = Template.getTemplate(mdata.modelName, platformType);
-            let element = DSLTreeTransfer.parse(template, structure, platformType);
-            Object.assign(element, {
-                styles: styles || {},
-                modelName,
-                constraints: constraints || {}
-            });
+    /**
+     * 解析模型节点
+     */
+    static _parseWidgetNode(node, dslTree, platformType) {
+        // let styles = {};
+        // // 单节点才获取样式,否则node上没有样式
+        // if (jsonNode['0'] && !jsonNode['1']) {
+        //     styles = jsonNode['0'].styles;
+        // }
+
+        let matchData = dslTree.getModelData(node.id);
+        return walkModel(matchData,function(mdata,structure) {
+            let template = Template.getTemplate(mdata.modelName,platformType);
+            let element = DSLTreeTransfer.parse(template, structure,platformType);
+            Object.assign(element,getAttr(matchData,['zIndex','abX','abY','abXops','abYops','constraints','width','height','canLeftFlex','canRightFlex']));
+            
             return element;
-        }, node);
+        });
     }
     static addPrefix(node) {
         if (node.id.indexOf('ts-') != 0) {
@@ -132,6 +115,7 @@ class DSLTreeProcessor {
 }
 // 往外走
 function walkout(node, handler) {
+    node.beautyClass = "a";
     if (!node.children || !node.children.length) return;
     node.children.map(n => {
         walkout(n, handler);
@@ -140,17 +124,37 @@ function walkout(node, handler) {
     if (node.root) handler(node);
 }
 // 遍历matchData
-function walkModel(matchData, handler, node) {
+function walkModel(matchData,handler) {
     let structure = matchData.getMatchNode();
-    if (~matchData.modelName.indexOf('em1') && node && node.children) Object.assign(structure[0], {
-        children: node.children
-    });
+    // _debuggerByid('B445DFC5-B0F5-48B4-A679-A5A4CDFA7D9F',structure);
     for (let key in structure) {
-        if (structure[key] && structure[key].constructor.name === 'MatchData') {
-            structure[key] = walkModel(structure[key], handler) // 如果是个模型节点，则继续拆解
+        if (structure[key].constructor.name === 'MatchData') {
+            structure[key] = walkModel(structure[key],handler) // 如果是个模型节点，则继续拆解
         }
     }
-    return handler(matchData, structure); // 处理节点
+    return handler(matchData,structure); // 处理节点
+}
+function getAttr(node,keys,move = false) {
+    let obj = {};
+    if (!node) return obj;
+    for (let key of keys) {
+        if (isValue(node[key])) {
+            obj[key] = node[key];
+            if(move) delete node[key];
+        }
+    }
+    return obj;
+}
+function isValue(val) {
+    if (!val) return false;
+    if (typeof val === 'object') {
+        if (Array.isArray(val)) return !!val.length
+        else return !!Object.keys(val).length
+    } else return true;
+}
+function _debuggerByid(id,structure) {
+    let n = Object.values(structure).find(n => ~n.id.indexOf(id))
+    if (n) debugger
 }
 module.exports = {
     parse
