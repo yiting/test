5c1724435cd036e8e69b6e99cdc31511a5a1f36d
alltasxiao <alltasxiao@tencent.com>
2019-01-10 20:46:14 +0800

    更新

--- code/server_modules/template/html/widgets/wg3/m3.js
+++ code/server_modules/template/html/widgets/wg3/m3.js
@@ -4,7 +4,7 @@ const WG3M3 = {
   desc: "组件模型(3元素)：上图片+下文本1+下文本2",
   template: `
       <div :class="wg3-m3">
-          <div :ref="0" :class="img"></div>
+          <span :ref="0" :class="img"></span>
           <p :ref="1" :class="text1"></p>
           <p :ref="2" :class="text2"></p>
       </div>`
