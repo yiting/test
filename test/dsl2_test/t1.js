087a3095e8f5ef4c84830c5d12ce3baa9e409e2f
chrisschen <chrisschen@tencent.com>
2019-01-11 18:34:08 +0800

    1，更新测试

--- test/dsl2_test/t1.js
+++ test/dsl2_test/t1.js
@@ -1067,9 +1067,10 @@ let cycle = [[sim[0]], [sim[1]], [sim[2]], [sim[3]], [sim[4]]];
 let node = Tree.createCycleData(jsonTree, cycle, 1234);
 //console.log(node);
 let renderData = node.getRenderData();
-console.log(renderData);
-console.log('------------------');
-console.log(renderData.children[0]);
+// console.log(renderData);
+// console.log('------------------');
+// console.log(renderData.children[0]);
+console.log(dslTree.getRenderData().children[1].children[0]);
 
 // let htmlStr = render.getTagString();
 // let cssStr = render.getStyleString();
