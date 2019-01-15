645fce7e38cfa23d3cde5e3ba21ca15645b3a617
chironyang <chironyang@tencent.com>
2019-01-15 17:56:23 +0800

    更新按钮点击态

--- code/server_modules/template/html/widgets/wg2/m6.js
+++ code/server_modules/template/html/widgets/wg2/m6.js
@@ -3,7 +3,7 @@ const WG2M6 = {
   name: "wg2-m6",
   desc: "组件模型(2元素)：QShape+文本",
   template: `
-    <div ref="0" :class="button">
+    <div :ref="0" :class="button">
         <span :ref="1" :class="text"></span>
     </div>`
 };
