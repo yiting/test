007f3e374da70bf0e8fc7a8155896c88e9954d0f
chironyang <chironyang@tencent.com>
2019-08-06 14:41:35 +0800

    fix: 回复默认字符串

--- src/core/dsl_service_ts/template/ark/widgets/wg0/m1.ts
+++ src/core/dsl_service_ts/template/ark/widgets/wg0/m1.ts
@@ -2,16 +2,7 @@ import ArkTemplate from '../../arkTemplate';
 class LAYER extends ArkTemplate {
   constructor(...args: any[]) {
     super(...args);
-    let tpl = `<View size margin anchors`;
-    if (this._renderData.type == 'QBody') {
-      tpl += `metadatatype="shareData">
-          <Event>
-          <OnSetValue value="app.OnSetMetaData"/>
-          <OnResize value="app.OnResize"/>
-      </Event>`;
-    } else {
-      tpl += '>';
-    }
+    let tpl = `<View size margin anchors>`;
     if (this.path) {
       tpl += `<Image size value="${
         this.path
