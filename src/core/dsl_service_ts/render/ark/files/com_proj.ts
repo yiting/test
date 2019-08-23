b8492540967028908cb1a7080504f1b5c136b918
chironyang <chironyang@tencent.com>
2019-08-23 10:19:24 +0800

    fix: 修改ark代码文件内容
    # 原内容代码路径错误

--- src/core/dsl_service_ts/render/ark/files/com_proj.ts
+++ src/core/dsl_service_ts/render/ark/files/com_proj.ts
@@ -5,30 +5,28 @@ export default (list: string[]) => {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <Project version="3">
     <Files>
-      <File path="/com.cust"></File>
-      <File path="/com.proj"></File>
-      <File path="/src/app.lua"></File>
-      <File path="/src/app.xml"></File>
-      <File path="/src/font.xml"></File>
-      <File path="/src/manifest.xml"></File>
-      <File path="/src/script/script/ams.lua"></File>
-      <File path="/src/script/animate.lua"></File>
-      <File path="/src/script/domainList.lua"></File>
-      <File path="/src/script/love.lua"></File>
-      <File path="/src/script/p_util.lua"></File>
-      <File path="/src/util/README.md"></File>
-      <File path="/src/util/cache.lua"></File>
-      <File path="/src/util/eventBus.lua"></File>
-      <File path="/src/util/net.lua"></File>
-      <File path="/src/util/report.lua"></File>
-      <File path="/src/util/animate.lua"></File>
-      <File path="/src/util/domainList.lua"></File>
-      <File path="/src/util/layout.lua"></File>
-      <File path="/src/util/qq.lua"></File>
-      <File path="/src/util/util.lua"></File>
-      <File path="/src/view/dataReport.lua"></File>
-      <File path="/src/view/index.lua"></File>
-      <File path="/src/view/index.xml"></File>
+      <File path="app.lua"></File>
+      <File path="app.xml"></File>
+      <File path="font.xml"></File>
+      <File path="manifest.xml"></File>
+      <File path="script/script/ams.lua"></File>
+      <File path="script/animate.lua"></File>
+      <File path="script/domainList.lua"></File>
+      <File path="script/love.lua"></File>
+      <File path="script/p_util.lua"></File>
+      <File path="util/README.md"></File>
+      <File path="util/cache.lua"></File>
+      <File path="util/eventBus.lua"></File>
+      <File path="util/net.lua"></File>
+      <File path="util/report.lua"></File>
+      <File path="util/animate.lua"></File>
+      <File path="util/domainList.lua"></File>
+      <File path="util/layout.lua"></File>
+      <File path="util/qq.lua"></File>
+      <File path="util/util.lua"></File>
+      <File path="view/dataReport.lua"></File>
+      <File path="view/index.lua"></File>
+      <File path="view/index.xml"></File>
         ${files.join('')}
     </Files>
   </Project>`;
