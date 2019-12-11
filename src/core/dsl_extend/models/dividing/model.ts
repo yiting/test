5c1ce150b28ffb2777ea5a458267d7624e596fe9
chironyang <chironyang@tencent.com>
2019-12-11 20:53:09 +0800

    fix: 1. 根据Design-JSON结构变化，修改模型数据判断逻辑；2. 修复样式命名规则；3. 修复相似判断逻辑

--- src/core/dsl_extend/models/dividing/model.ts
+++ src/core/dsl_extend/models/dividing/model.ts
@@ -10,7 +10,7 @@ class Dividing extends Model {
   static regular(node: any) {
     Dividing.designWidth = Store.get('designWidth');
     return (
-      node.type == Dictionary.type.QImage &&
+      (node.type == 'QImage' || node.type == 'QShape') &&
       node.width > Dividing.designWidth * 0.7 &&
       node.height < Dividing.designWidth * 0.04266
     );
--- src/core/dsl_extend/models/layer/model.ts
+++ src/core/dsl_extend/models/layer/model.ts
@@ -13,40 +13,40 @@ class Layer extends Model {
     return node.type == 'QLayer';
   }
   public isSimilarWith(target: any) {
-    let ErrorCoefficient = Store.get('errorCoefficient') || 0;
-    let a = this,
-      b = target;
+    // let ErrorCoefficient = Store.get('errorCoefficient') || 0;
+    // let a = this,
+    //   b = target;
     // 布局相似
-    let isSimilar =
-      // 水平中线对齐
-      (Math.abs(a.abYops - a.abY - b.abYops + b.abY) < ErrorCoefficient &&
-        // 左、中、右对齐
-        (Math.abs(a.abX - b.abX) < ErrorCoefficient ||
-          Math.abs(a.abXops - b.abXops) < ErrorCoefficient ||
-          Math.abs(a.centerAbX - b.centerAbX) < ErrorCoefficient)) ||
-      // 垂直中线对齐
-      (Math.abs(a.abXops - a.abX - b.abXops + b.abX) < ErrorCoefficient &&
-        // 上、中、下对齐
-        (Math.abs(a.abY - b.abY) < ErrorCoefficient ||
-          Math.abs(a.abYops - b.abYops) < ErrorCoefficient ||
-          Math.abs(a.centerAbY - b.centerAbY) < ErrorCoefficient));
-    if (isSimilar) {
-      let meta = Methods.filterAbsNode(this.children);
-      let targ = Methods.filterAbsNode(target.children);
-      let leng = meta.length;
-      if (meta.length != targ.length) {
+    // let isSimilar =
+    //   // 水平中线对齐
+    //   (Math.abs(a.abYops - a.abY - b.abYops + b.abY) < ErrorCoefficient &&
+    //     // 左、中、右对齐
+    //     (Math.abs(a.abX - b.abX) < ErrorCoefficient ||
+    //       Math.abs(a.abXops - b.abXops) < ErrorCoefficient ||
+    //       Math.abs(a.centerAbX - b.centerAbX) < ErrorCoefficient)) ||
+    //   // 垂直中线对齐
+    //   (Math.abs(a.abXops - a.abX - b.abXops + b.abX) < ErrorCoefficient &&
+    //     // 上、中、下对齐
+    //     (Math.abs(a.abY - b.abY) < ErrorCoefficient ||
+    //       Math.abs(a.abYops - b.abYops) < ErrorCoefficient ||
+    //       Math.abs(a.centerAbY - b.centerAbY) < ErrorCoefficient));
+    // if (isSimilar) {
+    let meta = Methods.filterAbsNode(this.children);
+    let targ = Methods.filterAbsNode(target.children);
+    let leng = meta.length;
+    if (meta.length != targ.length) {
+      return false;
+    }
+    for (let i = 0; i < leng; i++) {
+      let a = meta[i],
+        b = targ[i];
+      if (!a.isSimilarWith(b)) {
         return false;
       }
-      for (let i = 0; i < leng; i++) {
-        let a = meta[i],
-          b = targ[i];
-        if (!a.isSimilarWith(b)) {
-          return false;
-        }
-      }
-      return true;
     }
-    return false;
+    return true;
+    //   }
+    //   return false;
   }
 }
 
